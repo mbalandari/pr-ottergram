9a99a9c83cc25b99a7f6c8c0937ca1d4d43f0393
codingMaz <m.balandari.toroghi@gmail.com>
2023-12-12 20:11:04 +0330

    First

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import Barry from './otters/otter1.jpg';
+import Robin from './otters/otter2.jpg';
+import Maurice from './otters/otter3.jpg';
+import Lesley from './otters/otter4.jpg';
+import Barbara from './otters/otter5.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className='header-component'>
+        <h1>Ottergram</h1>
       </header>
+      <ul className='post-list'>
+        <li className='post-component'>
+          <button>
+            <img src={Barry} alt="Barry" />
+            <p>Barry</p>
+          </button>
+        </li>
+        <li className='post-component'>
+          <button>
+            <img src={Robin} alt="Robin" />
+            <p>Robin</p>
+          </button>
+        </li>
+        <li className='post-component'>
+          <button>
+            <img src={Maurice} alt="Maurice" />
+            <p>Maurice</p>
+          </button>
+        </li>
+        <li className='post-component'>
+          <button>
+            <img src={Lesley} alt="Lesley" />
+            <p>Lesley</p>
+          </button>
+        </li>
+        <li className='post-component'>
+          <button>
+            <img src={Barbara} alt="Barbara" />
+            <p>Barbara</p>
+          </button>
+        </li>
+      </ul>
     </div>
   );
 }
