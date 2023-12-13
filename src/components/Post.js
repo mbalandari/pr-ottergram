58ba59edb92146215f4e47d845689ac35618d34d
codingMaz <m.balandari.toroghi@gmail.com>
2023-12-14 03:27:10 +0330

    add enter and leave handlers

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,9 +3,21 @@ const Post = ({ image, name }) => {
         return console.log('You clicked ' + otterName)
     }
 
+    const handleEnter = (otterName) => {
+        return console.log('Hello, ' + otterName)
+    }
+
+    const handleExit = (otterName) => {
+        return console.log('Goodbye, ' + otterName)
+    }
+
     return (
         <li className='post-component'>
-            <button onClick={() => handleClick(name)}>
+            <button
+                onClick={() => handleClick(name)}
+                onMouseEnter={() => handleEnter(name)}
+                onMouseLeave={() => handleExit(name)}
+            >
                 <img src={image} alt={name} />
                 <p className="post-name">{name}</p>
             </button>
